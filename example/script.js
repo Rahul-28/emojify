82eb4a055713833e1817025243785e5563de7fa1
Rahul-28 <rahulrajeshrr365@gmail.com>
2025-03-24 21:36:17 +0530

    add example outputs for emoji library functions in script.js

--- example/script.js
+++ example/script.js
@@ -3,6 +3,7 @@ import * as emoji from 'emojify';
 const temp = new emoji.EmojiData();
 
 const str = 'I heart Pizza';
+
 const splitted = str.split(' ');
 splitted.forEach((part) => {
   const emojiIm = temp.getEmojiByName(part);
@@ -12,7 +13,40 @@ splitted.forEach((part) => {
     console.log(part);
   }
 });
-console.log();
+// Output:
+// I
+// ❤️
+// 🍕
+
 console.log(temp.getImageData('smile'));
+// Output: {
+// {
+//     x: 53.333333333333336,
+//     y: 83.33333333333334,
+//     sheetSizeX: 6100,
+//     sheetSizeY: 6100,
+//     image: '1f604.png'
+//   }
+
 console.log(temp.getEmojiByName('heart_eyes'));
+// Output: {
+//     name: 'SMILING FACE WITH HEART-SHAPED EYES',
+//     unified: '1F60D',
+//     image: '1f60d.png',
+//     sheet_x: 32,
+//     sheet_y: 59,
+//     short_name: 'heart_eyes',
+//     short_names: [ 'heart_eyes' ],
+//     category: 'Smileys & Emotion',
+//     subcategory: 'face-affection',
+//     char: '😍'
+//   }
+
 console.log(temp.getImageDataWithColon(':smile:'));
+// Output: {
+//  x: 53.333333333333336,
+//  y: 83.33333333333334,
+//  sheetSizeX: 6100,
+//  sheetSizeY: 6100,
+//  image: '1f604.png'
+// }
